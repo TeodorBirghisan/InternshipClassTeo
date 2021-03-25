172eb37e4635fd0205e5f02744b17cf77e7ebe44
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-03-25 13:10:47 +0200

    Added jquery script to Privacy.cshtml

--- wwwroot/js/addNewcomer.js
+++
@@ -1,9 +0,0 @@
-﻿function addNewcomer() {
-    var list = document.getElementById("list");
-    var newcomer = document.getElementById("newcomer");
-    var node = document.createElement("li");
-    node.innerText = newcomer.value;
-    list.appendChild(node);
-}
-
-document.getElementById("add").onclick = function () { addNewcomer() };
