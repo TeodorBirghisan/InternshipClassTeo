453311c78aea40ef044ff71634bb0fdc9ebe49d3
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-03-25 13:46:59 +0200

    ADD Api methods to AddMember

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,13 @@
 ﻿using System.Diagnostics;
+using InternshipClass.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using InternshipClass.Models;
 
 namespace InternshipClass.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly InternshipClassList _internshipClass; 
+        private readonly InternshipClassList _internshipClass;
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -24,6 +24,19 @@ namespace InternshipClass.Controllers
         public IActionResult Privacy()
         {
             return View(_internshipClass);
+        }
+
+        [HttpDelete]
+        public void RemoveMember(int index)
+        {
+            _internshipClass.Members.RemoveAt(index);
+        }
+
+        [HttpGet]
+        public string AddMember(string member)
+        {
+            _internshipClass.Members.Add(member);
+            return member;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- wwwroot/js/Index.js
+++ wwwroot/js/Index.js
@@ -3,11 +3,18 @@ $(document).ready(function () {
     // see https://api.jquery.com/click/
     $("#add").click(function () {
         var newcomerName = $("#newcomer").val();
+        $.ajax({
+            url: `/Home/AddMember?member=${newcomerName}`,
+            success: function (data) {
+                // Remember string interpolation
+                $("#list").append(`<li>${data}</li>`);
 
-        // Remember string interpolation
-        $("#list").append(`<li>${newcomerName}</li>`);
-
-        $("#newcomer").val("");
+                $("#newcomer").val("");
+            },
+            error: function (data) {
+                alert(`Failed to add ${newcomerName}`);
+            },
+        });
     })
 
     $("#clear").click(function () {
