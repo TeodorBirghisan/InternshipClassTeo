b583f8674778fb203523ab7565056d1377a428e2
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-03-29 13:38:48 +0300

    Added refresh function

--- wwwroot/js/WeatherForecast.js
+++ wwwroot/js/WeatherForecast.js
@@ -1,15 +1,20 @@
-﻿$(document).ready(function () {
-    $.ajax({
-        url: '/WeatherForecast',
-        success: function (data) {
-            let tommorrow = data[0];
-            let date = new Date(tommorrow.date).toDateString();
-            $('#date').text(date);
-            $('#temperature').text(tommorrow.temperatureC + ' C');
-            $('#summary').text(tommorrow.summary);
-        },
-        error: function (data) {
-            alert('Failed to load data');
-        }
+﻿function refreshWeatherForecast() {
+    $(document).ready(function () {
+        $.ajax({
+            url: '/WeatherForecast',
+            success: function (data) {
+                let tommorrow = data[0];
+                let date = new Date(tommorrow.date).toDateString();
+                $('#date').text(date);
+                $('#temperature').text(tommorrow.temperatureC + ' C');
+                $('#summary').text(tommorrow.summary);
+            },
+            error: function (data) {
+                alert('Failed to load data');
+            }
+        });
     });
-});
+}
+
+
+setInterval(refreshWeatherForecast, 3000);
