86776297f97e0d35e594e9c1cb856d1f38162558
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-04-02 11:26:50 +0300

    Change to correct namespace

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using TestDb.Data;
+using InternshipClass.Data;
 
 namespace InternshipClass
 {
