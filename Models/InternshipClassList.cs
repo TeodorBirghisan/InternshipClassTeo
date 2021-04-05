452c2cb20542cd960cb3d8b708266d350eeec181
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-04-05 15:28:05 +0300

    InternshipClassList

--- Models/InternshipClassList.cs
+++ Models/InternshipClassList.cs
@@ -7,18 +7,21 @@ namespace InternshipClass.Models
 {
     public class InternshipClassList
     {
-        private readonly List<string> _members;
+        private List<Intern> _members;
 
         public InternshipClassList()
         {
-            _members = new List<string>
+            _members = new List<Intern>
             {
-                "Borys",
-                "Liova",
-                "Orest",
+                new Intern { Name = "Borys", RegistrationDateTime = DateTime.Parse("2021-04-01"), Id = 1 },
+                new Intern { Name = "Liova", RegistrationDateTime = DateTime.Parse("2021-04-01"), Id = 2 },
+                new Intern { Name = "Orest", RegistrationDateTime = DateTime.Parse("2021-03-31"), Id = 3 },
             };
         }
 
-        public IList<string> Members => _members;
+        public IList<Intern> Members
+        {
+            get { return _members; }
+        }
     }
 }
