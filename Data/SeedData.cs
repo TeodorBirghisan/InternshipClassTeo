e7d69847692c96f59f1f929669550e1acfe10859
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-04-05 15:26:09 +0300

    Add Seed Data

---
+++ Data/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using InternshipClass.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternshipClass.Data
+{
+    public class SeedData
+    {
+        public static void Initialize(InternDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Interns.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var interns = new Intern[]
+            {
+                new Intern { Id = 1, Name = "AAAAA", RegistrationDateTime = DateTime.Parse("2021-04-01") },
+                new Intern { Id = 2, Name = "BBBBB", RegistrationDateTime = DateTime.Parse("2021-04-01") },
+                new Intern { Id = 3, Name = "CCCCC", RegistrationDateTime = DateTime.Parse("2021-03-31") },
+            };
+
+            context.Interns.AddRange(interns);
+            context.SaveChanges();
+        }
+    }
+}
