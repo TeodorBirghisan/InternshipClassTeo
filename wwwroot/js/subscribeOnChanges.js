4848ffcf4e018fd1db275b6aa51cfe99b9034043
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-04-12 14:00:58 +0300

    Add signalR and subscribeOnChanges.js to Index

---
+++ wwwroot/js/subscribeOnChanges.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/messagehub").build();
+
+connection.on("AddMemeber", function (name, id) {
+    alert(`User ${name} with id = ${id}`);
+});
+
+connection.start().then(function () {
+    console.log("Connection established");
+}).catch(function (err) {
+    return console.error(err.toString());
+});
