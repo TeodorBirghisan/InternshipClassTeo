96f92d37928f25884a7bdd298ee623d06c1e9d3a
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-04-13 13:05:24 +0300

    Corrected the name

--- wwwroot/js/subscribeOnChanges.js
+++ wwwroot/js/subscribeOnChanges.js
@@ -2,7 +2,7 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/messagehub").build();
 
-connection.on("AddMemeber", function (name, id) {
+connection.on("AddMember", function (name, id) {
     $("#list").append(`<li class="member">
         <span class="name">${name}</span><span class="delete fa fa-remove"></span><i class="startEdit fa fa-pencil" data-toggle="modal" data-target="#editClassmate"></i>
     </li>`);
