718a594e9fd4322e11cbc1c968d7b7c30afdc189
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-04-14 10:19:56 +0300

    Removed useless HomeController Rest Method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,34 +26,6 @@ namespace InternshipClass.Controllers
             this.messageService = messageService;
         }
 
-        [HttpDelete]
-        public void RemoveMember(int index)
-        {
-            intershipService.RemoveMember(index);
-        }
-
-        [HttpGet]
-        public Intern AddMember(string memberName)
-        {
-            Intern intern = new Intern();
-            intern.Name = memberName;
-            intern.RegistrationDateTime = DateTime.Now;
-            var newMember = intershipService.AddMember(intern);
-            hubContext.Clients.All.SendAsync("AddMember", newMember.Name, newMember.Id);
-            return newMember;
-        }
-
-        [HttpPut]
-        public void UpdateMember(int index, string memberName)
-        {
-            Intern intern = new Intern();
-            intern.Id = index;
-            intern.Name = memberName;
-            intern.RegistrationDateTime = DateTime.Now;
-            intershipService.UpdateMember(intern);
-        }
-
-
         public IActionResult Index()
         {
             var interns = intershipService.GetMembers();
