da43c429d3ec21fb6421f04c20b804ccb64cacce
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-04-19 14:05:03 +0300

    Populate DB if is empty

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -15,30 +15,30 @@ namespace InternshipClass.Data
         {
             context.Database.Migrate();
 
-            if (context.Interns.Any())
+            if (!context.Locations.Any())
             {
-                return;   // DB has been seeded
-            }
-
-
-            var locations = new Location[]
-            {
+                var locations = new Location[]
+                {
                 defaultLocation = new Location { Name = "Kyiv", NativeName = "Київ", Longitude = 30.5167, Latitude = 50.4333, },
                 new Location { Name = "Brasov", NativeName = "Braşov", Longitude = 25.3333, Latitude = 45.75, },
-            };
+                };
+                context.Locations.AddRange(locations);
+                context.SaveChanges();
+            }
 
-            context.Locations.AddRange(locations);
-
-            var interns = new Intern[]
+            if (!context.Interns.Any())
             {
-                new Intern { Name = "AAAAA", RegistrationDateTime = DateTime.Parse("2021-04-01"), Location = defaultLocation },
-                new Intern { Name = "BBBBB", RegistrationDateTime = DateTime.Parse("2021-04-01"), Location = defaultLocation },
-                new Intern { Name = "CCCCC", RegistrationDateTime = DateTime.Parse("2021-03-31"), Location = defaultLocation },
-            };
+                var interns = new Intern[]
+                {
+                        new Intern { Name = "AAAAA", RegistrationDateTime = DateTime.Parse("2021-04-01"), Location = defaultLocation },
+                        new Intern { Name = "BBBBB", RegistrationDateTime = DateTime.Parse("2021-04-01"), Location = defaultLocation },
+                        new Intern { Name = "CCCCC", RegistrationDateTime = DateTime.Parse("2021-03-31"), Location = defaultLocation },
+                };
 
-            context.Interns.AddRange(interns);
+                context.Interns.AddRange(interns);
 
-            context.SaveChanges();
+                context.SaveChanges();
+            }
         }
     }
 }
