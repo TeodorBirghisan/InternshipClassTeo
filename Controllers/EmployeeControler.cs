354028ea7b4e546a0d26b6d156a6b89468d046e6
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-04-27 12:57:44 +0300

    POST GET and GETone for employee

---
+++ Controllers/EmployeeControler.cs
@@ -0,0 +1,40 @@
+﻿using InternshipClass.Models;
+using InternshipClass.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternshipClass.Controllers
+{
+    [Route("employee/[controller]")]
+    [ApiController]
+    public class EmployeeControler : ControllerBase
+    {
+        private readonly EmployeeDbService employeeDbService;
+
+        public EmployeeControler(EmployeeDbService employeeDbService)
+        {
+            this.employeeDbService = employeeDbService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return employeeDbService.GetEmployees();
+        }
+
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return employeeDbService.GetEmployeeById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Employee employee)
+        {
+            employeeDbService.AddEmployee(employee);
+        }
+    }
+}
